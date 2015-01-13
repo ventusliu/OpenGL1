77634014e54931c6b75b29e518f72019c14809de
ventusliu <ventusliu@yahoo.com>
2015-01-13 16:42:40 +0000

    main updated

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 void drawTriangle();
 
-
 int main()
 {
     if(SDL_Init(SDL_INIT_VIDEO))
@@ -43,20 +42,36 @@ int main()
     glMatrixMode(GL_MODELVIEW);
     gluLookAt(2,2,2,0,0,0,0,1,0);
 
-    SDL_GL_SwapWindow(window);
+    bool quit=false;
+    SDL_Event event;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch( event.type)
+            {
+                case SDL_QUIT : quit = true; break;
+            }
+        }
 
-    void drawTriangle();
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawTriangle();
+        SDL_GL_SwapWindow(window);
 
-    SDL_Delay(10000);
+    }
+
+
 }
 
 void drawTriangle()
 {
+    static int rot=0;
     glPushMatrix();
+        glRotated(++rot,0,1,0);
         glBegin(GL_TRIANGLES);
             glColor3f(1.0f,0.0,0.0f);
             glVertex3f(0.0f,1.0f,0.0f);
-            glColor3f(0.0f,0.0f,1.0f);
+            glColor3f(0.0f,1.0f,0.0f);
             glVertex3f(0.0f,0.0f,1.0f);
             glColor3f(0.0f,0.0f,1.0f);
             glVertex3f(01.0f,-1.0f,0.0f);
